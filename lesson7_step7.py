8ef44a6b80e9ab10662a677a1f60b95f88ed6276
alexandr.miheev <zoom906@gmail.com>
2021-02-12 01:44:40 +0300

    Modified files and add new lesson files

--- lesson7_step7.py
+++ lesson7_step7.py
@@ -2,26 +2,27 @@ from selenium import webdriver
 import time
 import math
 
+    # Открыть страницу http://suninjuly.github.io/get_attribute.html
 try:
-    link = "http://suninjuly.github.io/math.html"
+    link = "http://suninjuly.github.io/get_attribute.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    x_element = browser.find_element_by_id("input_value")
-    x = int(x_element.text)
+    # Найти на ней элемент-картинку/ Взять у этого элемента значение атрибута valuex
+    x_element = browser.find_element_by_id("treasure").get_attribute("valuex")
+    x = int(x_element)
     print(x)
-
 
     # Посчитать математическую функцию от x, Ввести ответ в текстовое поле.
     def calc(x):
         return str(math.log(abs(12 * math.sin(int(x)))))
 
     y = calc(x)
-    print(y)
 
+    # Ввести ответ в текстовое поле. Отметить checkbox "Подтверждаю, что являюсь роботом". Выбрать radiobutton "Роботы рулят!".
     browser.find_element_by_id('answer').send_keys(y)
-    browser.find_element_by_css_selector("[for='robotCheckbox']").click()
-    browser.find_element_by_css_selector("[for='robotsRule']").click()
+    browser.find_element_by_id('robotCheckbox').click()
+    browser.find_element_by_id('robotsRule').click()
 
     # Отправляем заполненную форму
     browser.find_element_by_css_selector("button.btn").click()
